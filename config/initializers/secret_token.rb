156f697de9cbf29f4a4c773e6c6794e2bc01ac82
ANKUR <Ankur.Gupta@citygridmedia.com>
2013-08-15 10:52:54 +0530

    Empty Rails project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livemap::Application.config.secret_token = 'e1afaa82c02c93bf654b021109d6919b30df71f0dc1e909dd1f7a5c5679b209de2f538730d9957da7a4f9e62995d1002c1a834050ebf7743c5a353266067c6b8'
